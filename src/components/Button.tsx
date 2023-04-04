592ed099c79e8d773b22847e06d55716e8eab6de
shimaxu <shimax15@gmail.com>
2023-04-04 23:06:45 +0500

    button component color refactored

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -19,26 +19,7 @@ const Button = ({
   if (size == "l") classes = "px-3 py-2 text-sm";
   if (size == "xl") classes = "px-3.5 py-2.5 text-sm";
 
-  if (color == "indigo")
-    classes +=
-      " bg-indigo-600 hover:bg-indigo-500 focus-visible:outline-indigo-600";
-
-  if (color == "gray")
-    classes += " bg-gray-600 hover:bg-gray-500 focus-visible:outline-gray-600";
-
-  if (color == "emerald")
-    classes +=
-      " bg-emerald-600 hover:bg-emerald-500 focus-visible:outline-emerald-600";
-
-  if (color == "blue")
-    classes += " bg-blue-600 hover:bg-blue-500 focus-visible:outline-blue-600";
-
-  if (color == "yellow")
-    classes +=
-      " bg-yellow-600 hover:bg-yellow-500 focus-visible:outline-yellow-600";
-
-  if (color == "red")
-    classes += " bg-red-600 hover:bg-red-500 focus-visible:outline-red-600";
+  classes += ` bg-${color}-600 hover:bg-${color}-500 focus-visible:outline-${color}-600`;
 
   return (
     <button
