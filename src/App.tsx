627b325962fee02d2846b8bbf66841a40ead25ec
shimaxu <shimax15@gmail.com>
2023-04-14 00:38:55 +0500

    chakra basic grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { Button } from "@chakra-ui/react";
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 const App = () => {
   return (
-    <Button
-      colorScheme="teal"
-      variant="outline"
-      size="lg"
-      onClick={() => console.log("clicked")}
-    >
-      Button
-    </Button>
+    <Grid
+      templateAreas={{ 
+        base: `"nav" "main"`, 
+        lg: `"nav nav" "aside main"` 
+      }}>
+      <GridItem bg="orange.300" area="nav">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg="pink.300" area="aside">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem bg="green.300" area="main">
+        Main
+      </GridItem>
+    </Grid>
   );
 };
 
